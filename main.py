676860a017e6f1369819019c7e6f2d5ef8d2ab0c
Duwik <norkholifan2@gmail.com>
2026-05-07 14:31:34 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+print("Hello, World!")
+
+# ini adalah komen pada 
+
+# kita bisa mengcompile pthon ke
+# yang namanya bytecode 
+# cara mengcompile, buka terminal dan ketik
+# python3 -m py_compile main.py
+
+# variable
+# variable adalah tempat untuk menyimpan data
+ # a adalah variable yang menyimpan data 10
+ # = adalah operator assignment untuk memberikan nilai ke variable
+ # 10 adalah value atau nilai yang disimpan dalam variable a
+ 
+a = 10 
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 20 # harus mengguanakan underscore untuk memisahkan kata
+juta10 = 10000000 # tidak boleh diawali dengan angka
+nilaiZ = 30 # boleh menggunakan huruf kapital untuk memisahkan kata, tapi tidak disarankan
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 5 # kita bisa mengubah nilai variable a menjadi 5
+print("Nilai a = ", a) # nilai a sekarang adalah 5
+
+# assigment indierect
+b = a # variable b sekarang menyimpan nilai yang sama dengan variable a
+print("Nilai b = ", b) # nilai b adalah 5, karena menyimpan
